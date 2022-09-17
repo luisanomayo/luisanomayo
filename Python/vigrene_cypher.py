b1edad4e69b388b070fc7adb7486aff0c1ccfa06
Ame Avan-Nomayo <36841345+luisanomayo@users.noreply.github.com>
2022-09-17 18:24:46 +0100

    Update vigrene_cypher.py

--- Python/vigrene_cypher.py
+++ Python/vigrene_cypher.py
@@ -1,39 +1,23 @@
-#key = 'hello'
-#key_list = list(key)
-#alpha_index = ' abcdefghijklmnopqrstuvwxyz'
-#alpha_cap_index = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-#key_index = []
-
-"""def finding_key_index (key):
-	for alpha in alpha_index:
-		index = 0
-		while index <= len(key_list) - 1:
-			if alpha == key_list[index]:
-				key_index.append (alpha_index.find(alpha))
-			index += 1
-	return key_index"""
-
-
-"""def finding_cap_key_index (key):
-	for alpha in alpha_cap_index:
-		index = 0
-		while index <= len(key_list) - 1:
-			if alpha == key_list[index]:
-				key_index.append (alpha_cap_index.find(alpha))
-			index += 1
-	return key_index"""
-
-#determine the index of the key
-
-#key = "HELLO"
-
-
+key = input ('Enter your Desired Key: ')
+plain_text = input('Enter The Text to be Encrypted:')
 alpha_lowcap =  ' abcdefghijklmnopqrstuvwxyz'
 alpha_cap = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-low_alpha = 'abcdefghijklmnopqrstuvwxyz'
-cap_alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'      
+#create index from the key given
+def finding_key_index (key):
+	key_list = list(key)
+	key_index = []
+	for char in key_list:
+		if char in alpha_cap:
+			key_index.append(alpha_cap.find(char))
+		elif char in alpha_lowcap:
+			key_index.append(alpha_lowcap.find(char))
+	return key_index
+  
+key_index = finding_key_index(key)
 
+
+#create index from the key given
 def finding_key_index (key):
 	key_list = list(key)
 	key_index = []
@@ -44,66 +28,49 @@ def finding_key_index (key):
 			key_index.append(alpha_lowcap.find(char))
 	return key_index
 
-key = 'amen'
-key_index = finding_key_index(key)
-plain_text = "hello everyone"
+#import cycle from itertools for infinite iteration over the key_index list
+#this is needed when `len(plain_text)` is longer than the lenght of the key given
+from itertools import cycle
 
-"""def vigrene_cypher(plain_text):
+#to iterate/loop over the key_index for as long as needed
+keyindex_cycle = cycle(key_index)
+
+#function to implement the move through the infinite iteration/loop
+def next_keyindex():
+	return next(keyindex_cycle)
+
+#main cypher function
+def vigrene_cypher():
+	#the cypher outputed when the function is called
 	encrypted_text = ''
+	#loop through the plain text inputted
 	for char in plain_text:
-		for index in key_index:
+		#to main any spaces included in the plain text
+		if char == ' ':
+			encrypted_text += ' '
+		else:
+			#check if each character in the plain text is lower case
 			if char in alpha_lowcap:
-				text_index = alpha_lowcap.index(char) + index
-				encrypted_text += alpha_lowcap[text_index]
-			elif char  in alpha_cap:
-				text_index = alpha_cap.index(char) + index
-				encrypted_text += alpha_cap[text_index]
-			else:
-				encrypted_text += char
-	return(encrypted_text)"""
-
-"""def vigrene_cypher():
-	encrypted_text = ''
-	for char in plain_text:
-		if char in alpha_lowcap:
-			for index in key_index:
-				text_index = alpha_lowcap.index(char)+index
-				if text_index <= 26:
-					encrypted_text += alpha_lowcap[text_index]
+				#call infinite loop function and place the current iterate in a variable 
+				#for use to update the index of each character in the plain text
+				keyindex = next_keyindex()
+				char_index = alpha_lowcap.index(char)
+				#create index for each encryption
+				encrypt_index = char_index + keyindex
+				#to resolve index out of range. 
+				if encrypt_index <= 26:
+					encrypted_text += alpha_lowcap[encrypt_index]
 				else:
 					encrypted_text += alpha_lowcap[26]
-				if len(encrypted_text) == len(plain_text):
-					break
-	return encrypted_text"""
-
-
-
-def vigrene_cypher ():
-  encrypted_word = ''
-  for char in plain_text:
-    
-    if char in alpha_lowcap:
-      
-      char_index = alpha_lowcap.index(char)
-      for index in key_index:
-      	
-      	encrypt_index = char_index + index
-      	if encrypt_index <= 26:
-     		
-      		encrypted_word += alpha_lowcap[encrypt_index]
-      	else:
-      		encrypted_word += alpha_lowcap[26]
-      
-    elif char in alpha_cap:
-    	char_index = alpha_cap.index(char)
-    	for index in key_index:
-    		encrypt_index = char_index +index
-    		if encrypt_index <= 26:
-    			encrypted_word += alpha_cap[encrypt_index]
-    		else:
-    			encrypted_word += alpha_cap[26]  
-    else:
-      encrypted_word += char
-  return encrypted_word
-
-print (vigrene_cypher()) 
+			#check if each character in the plain text is upper case		
+			elif char in alpha_cap:
+				char_index = alpha_cap.index(char)
+				encrypt_index = char_index + keyindex
+				if encrypt_index <= 26:
+					encrypted_text += alpha_cap[encrypt_index]
+				else:
+					encrypted_text += alpha_cap[26]
+			#to maintain non alphabets included in plain text		
+			else:
+				encrypted_text += char
+	return (encrypted_text)
